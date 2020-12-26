bdc142727ecdc28522bef3162630c29efe0d4587
Max Gonzih <gonzih@gmail.com>
2020-12-25 21:13:00 -0500

    Fix dem physics

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,8 @@ struct Player {
 impl Player {
     fn new() -> Self {
         return Self {
-            yaw: 0.0,
-            pitch: 0.0,
+            yaw: 34.0,
+            pitch: 12.0,
             sensitivity: 30.0,
         };
     }
@@ -30,14 +30,18 @@ impl Player {
 
 fn main() {
     App::build()
+        // resources
         .init_resource::<State>()
         .add_resource(Msaa { samples: 4 })
+        // plugins
         .add_plugin(RapierRenderPlugin)
         .add_plugin(RapierPhysicsPlugin)
         .add_plugins(DefaultPlugins)
         // .add_plugin(FrameTimeDiagnosticsPlugin::default())
         // .add_plugin(PrintDiagnosticsPlugin::default())
+        // setup
         .add_startup_system(setup.system())
+        // loop
         .add_system(rotate_player.system())
         .add_system(move_player.system())
         .run();
@@ -52,25 +56,24 @@ fn setup(
     commands
         // light
         .spawn(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, -4.0, 5.0)),
+            transform: Transform::from_translation(Vec3::new(0.0, 5.0, 0.0)),
             ..Default::default()
         })
         // camera
         .spawn(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(35.0, 35.0, 35.0)),
+            transform: Transform::from_translation(Vec3::new(35.0, -1.0, 35.0)),
             ..Default::default()
         })
         .with(Camera);
 
     let mut rng = StdRng::from_entropy();
     let player_handle = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
-    let player_body = RigidBodyBuilder::new_dynamic();
+    let player_body = RigidBodyBuilder::new_dynamic().translation(0.0, 100.0, 0.0);
     let player_collider = ColliderBuilder::cuboid(1.0, 1.0, 1.0);
 
-    let platform_translation = Vec3::new(-5.0, -60.0, -5.0);
-    let platform_handle = meshes.add(Mesh::from(shape::Cube { size: 100.0 }));
-    let platform_body = RigidBodyBuilder::new_static().translation(-5.0, -60.0, -5.0);
-    let platform_collider = ColliderBuilder::cuboid(100.0, 100.0, 100.0);
+    let platform_handle = meshes.add(Mesh::from(shape::Plane { size: 100.0 }));
+    let platform_body = RigidBodyBuilder::new_static().translation(-5.0, -5.0, -5.0);
+    let platform_collider = ColliderBuilder::cuboid(100.0, 0.0, 100.0);
 
     commands
         .spawn(PbrBundle {
@@ -83,7 +86,6 @@ fn setup(
                 ),
                 ..Default::default()
             }),
-            transform: Transform::from_translation(Vec3::new(0.0, 10.0, 0.0)),
             ..Default::default()
         })
         .with(Player::new())
@@ -100,7 +102,6 @@ fn setup(
                 ),
                 ..Default::default()
             }),
-            transform: Transform::from_translation(platform_translation),
             ..Default::default()
         })
         .with(platform_body)
@@ -119,28 +120,27 @@ fn rotate_player(
     mut state: ResMut<State>,
     time: Res<Time>,
     mouse_motion_events: Res<Events<MouseMotion>>,
-    // mut player_query: Query<(&mut Player, &mut Transform)>,
-    mut player_query: Query<(&mut Player)>,
+    mut player_query: Query<(&mut Player, &mut Transform)>,
     mut camera_query: Query<(&Camera, &mut Transform)>,
 ) {
     let mut delta = Vec2::zero();
     for event in state.mouse_motion_event_reader.iter(&mouse_motion_events) {
-        println!("{:?}", event);
         delta += event.delta;
     }
 
     let mut yaw_rad = 0.0;
     let mut pitch_rad = 0.0;
 
-    // for (mut player, mut transform) in player_query.iter_mut() {
-    for (mut player) in player_query.iter_mut() {
+    for (mut player, mut transform) in player_query.iter_mut() {
         player.yaw -= delta.x * player.sensitivity * time.delta_seconds();
         player.pitch += delta.y * player.sensitivity * time.delta_seconds();
         yaw_rad = player.yaw.to_radians();
         pitch_rad = player.pitch.to_radians();
-        // transform.rotation = Quat::from_axis_angle(Vec3::unit_y(), yaw_rad)
-        //     * Quat::from_axis_angle(-Vec3::unit_x(), pitch_rad);
+        transform.rotation = Quat::from_axis_angle(Vec3::unit_y(), yaw_rad);
+
+        println!("Player yaw = {}, pitch = {}", player.yaw, player.pitch);
     }
+
 
     for (_, mut transform) in camera_query.iter_mut() {
         transform.rotation = Quat::from_axis_angle(Vec3::unit_y(), yaw_rad)
@@ -152,7 +152,7 @@ fn move_player(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
     mut _materials: ResMut<Assets<StandardMaterial>>,
-    mut query: Query<(&Player, &mut Transform, &Handle<StandardMaterial>)>,
+    mut query: Query<(&Camera, &mut Transform)>,
 ) {
     let mut offset = Vec3::new(0.0, 0.0, 0.0);
     if keyboard_input.pressed(KeyCode::Left) {
@@ -170,12 +170,11 @@ fn move_player(
     if keyboard_input.pressed(KeyCode::Space) {
         offset.y += 10.0;
     }
-    if keyboard_input.pressed(KeyCode::C) {
-        offset.z -= 10.0;
+    if keyboard_input.pressed(KeyCode::LShift) {
+        offset.y -= 10.0;
     }
 
-    for (_player, mut transform, _material_handle) in query.iter_mut() {
-        // transform.rotate
+    for (_, mut transform) in query.iter_mut() {
         transform.translation += offset * time.delta_seconds();
     }
 }
